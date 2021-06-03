c11eaaae466be0bac74a00db1abafd02d31114d8
mramo21 <mantasramo@gmail.com>
2021-06-03 20:50:50 +0300

    learning js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio pomidoru darzas';
-h1DOM.classList.add('pavadinimas')
+let rezultatas = 0;
 
-const liDOM = document.querySelectorAll('li');
- for (let i=0;i<liDOM.length;i++) {
-     liDOM[i].innerText = "Pomidoras"
-     liDOM[i].classList.add('pomidoras')
- }
+const minusDOM = document.querySelector('.minus');
+const plusDOM = document.querySelector('.plus')
+const zeroDOM = document.querySelector('.number');
+const h1DOM = document.querySelector('h1')
+const h1DOMtext = h1DOM.innerText
+
+plusDOM.addEventListener('click', () => {
+    rezultatas = rezultatas+1;
+    h1DOM.innerText = 'Zaidimas progrese'
+    zeroDOM.innerText = rezultatas;
+})
+
+minusDOM.addEventListener('click', () => {
+    rezultatas = rezultatas - 1;
+    h1DOM.innerText = 'Zaidimas progrese'
+    zeroDOM.innerText = rezultatas;
+})
+
+zeroDOM.addEventListener('click', () => {
+    rezultatas = 0;
+    console.log(rezultatas)
+    h1DOM.innerText = h1DOMtext
+    zeroDOM.innerText = rezultatas;
+})
