9b6328aa9d82c4114596221317b80c631da7444e
mramo21 <mantasramo@gmail.com>
2021-06-04 20:51:46 +0300

    lessons continue

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
-let rezultatas = 0;
+const inputDOM = document.querySelector('input');
+const submitDOM = document.querySelector('button');
+const spanDOM = document.querySelector('span');
 
-const minusDOM = document.querySelector('.minus');
-const plusDOM = document.querySelector('.plus')
-const zeroDOM = document.querySelector('.number');
-const h1DOM = document.querySelector('h1')
-const h1DOMtext = h1DOM.innerText
 
-plusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas+1;
-    h1DOM.innerText = 'Zaidimas progrese'
-    zeroDOM.innerText = rezultatas;
-})
 
-minusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas - 1;
-    h1DOM.innerText = 'Zaidimas progrese'
-    zeroDOM.innerText = rezultatas;
-})
+submitDOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    spanDOM.innerText = inputDOM.value
 
-zeroDOM.addEventListener('click', () => {
-    rezultatas = 0;
-    console.log(rezultatas)
-    h1DOM.innerText = h1DOMtext
-    zeroDOM.innerText = rezultatas;
 })
