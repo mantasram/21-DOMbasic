7770dd2af2f76c652397375e7a5a1c15929e9825
mramo21 <mantasramo@gmail.com>
2021-06-05 13:54:31 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const inputDOM = document.querySelector('input');
-const submitDOM = document.querySelector('button');
-const spanDOM = document.querySelector('span');
+const vardasDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const patiekalasDOM = document.querySelector('#patiekalas')
+const desertasDOM = document.querySelector('#desertas');
+const sultysDOM = document.querySelector('#sultys');
+const giraDOM = document.querySelector('#gira');
+const pienasDOM = document.querySelector('#pienas');
+const buttonDOM = document.querySelector('button');
+const orderDOM = document.querySelector('.order');
+
+
+const item = ' ';
+
+function norinenori(item) {
+    return item.checked ? 'nori' : "nenori"
+}
 
 
 
-submitDOM.addEventListener('click', (event) => {
+
+
+buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
-    spanDOM.innerText = inputDOM.value
-
-})
+    orderDOM.innerText = `Uzsakovas vardu ${vardasDOM.value}  , ${norinenori(sriubaDOM)} sriubos,${norinenori(patiekalasDOM)} pagrindinio patiekalo, ${norinenori(desertasDOM)} deserto ir[gerymo pavadinimas]yra pasirinktas gerimas.`
+});
