a041c1899964513e33d585454df87eb55a469be5
mramo21 <mantasramo@gmail.com>
2021-06-08 22:09:03 +0300

    more js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,20 @@
-const vardasDOM = document.querySelector('#customer');
-const sriubaDOM = document.querySelector('#sriuba');
-const patiekalasDOM = document.querySelector('#patiekalas')
-const desertasDOM = document.querySelector('#desertas');
-const sultysDOM = document.querySelector('#sultys');
-const giraDOM = document.querySelector('#gira');
-const pienasDOM = document.querySelector('#pienas');
-const buttonDOM = document.querySelector('button');
-const orderDOM = document.querySelector('.order');
-const gerimaiDOM = document.querySelectorAll('input[name="gerimas"')
+function generuotiLenta (selector, number) {
+    const DOM = document.querySelector(selector);
+    if (!DOM) {
+        return false
+    }
+    if (number < 0 || number % 1 !== 0) {
 
+        return false
+    }
+    let HTML = '';
+    for (let i=1; i<=number; i++) {
+        HTML += `<div>${i}</div>`;
+    }
+    return DOM.innerHTML = HTML
 
-function aryraVardas (item) {
-    if (vardasDOM.value === '') 
-    return vardasDOM.value
-} 
-
-
-
-function norinenori(item) {
-    return item.checked ? 'nori' : "nenori"
 }
 
-function gerimasNori(DOMlist) {
-    for (const itemDOM of DOMlist) {
-        if (itemDOM.checked) {
-            return itemDOM.value;
-        }
-    }
-}
 
-buttonDOM.addEventListener('click', (event) => {
-    event.preventDefault();
-    let kreipinys = ''
-    if (vardasDOM.value === '') {
-        kreipinys =  'kuris nera pateikes vardo'
-    } else {
-        kreipinys = 'vardu ' + vardasDOM.value
-    }
 
-    let kreipinys2 = gerimasNori(gerimaiDOM) ? gerimasNori(gerimaiDOM) + ' yra pasirinktas gerimas.' : 'nepasirinko jokio gerimo'
-
-    orderDOM.innerText = `Uzsakovas ${kreipinys} ${aryraVardas(vardasDOM)}  , ${norinenori(sriubaDOM)} sriubos,${norinenori(patiekalasDOM)} pagrindinio patiekalo, ${norinenori(desertasDOM)}  deserto ir ${kreipinys2}`
-});
+console.log(generuotiLenta('.lenta', 4));
