cff9fabc847b89b62f399e87354794d165c6271b
mramo21 <mantasramo@gmail.com>
2021-06-10 21:40:31 +0300

    selectionjs

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-function renderNav() {
+const pasirinkimasDOM = document.querySelector('.option');
+const submitDOM = document.querySelector('button')
+const gyvunuList = [
+    { id: 'zuikis'},
+    { id: 'barsukas'},
+    { id: 'lape'},
+    { id: 'vilkas'},
+    { id: 'sernas'},
+    { id: 'stirna'},
+    { id: 'vovere'},
+]
+
+function renderSelect(selector, data){
+    const DOM = document.querySelector(selector);
+    let HTML = '';
+
+
+    for (const i in data){
+        const pet = data[i];
+        HTML += `<option value="${pet.id}">${pet.id}</option>`
+    }
+    DOM.insertAdjacentHTML('afterend', `<select name="pets" id="pets">${HTML}</select>`);
+
+    const petsDOM = document.querySelector('#pets');
+
+    submitDOM.addEventListener('click', (e) => {
+        e.preventDefault();
+        pasirinkimasDOM.innerText = petsDOM.value;
+    });
+
 
 }
 
-const menu = [
-    {
-        href: '#',
-        title: 'Home'
-    },
-    {
-        href: '#',
-        title: 'Services'
-    },
-    {
-        href: '#',
-        title: 'About us'
-    },
-    {
-        href: '#',
-        title: 'Contact us'
-    }
-];
+renderSelect('label', gyvunuList)
 
-renderNav('header', menu);
+
+
